4ac095819acfe9cc7dae2b637a260e7f7c24db45
EuniBAlp <eunice.ba43@gmail.com>
2024-07-14 21:56:26 +0000

    Trabajoalcorriente

---
+++ cast.py
@@ -0,0 +1,28 @@
+#Siempre que este algo entre comillas sera variable str
+numero= "147"
+print(numero)
+print(type(numero))
+#Se cambia la variable str a int
+numero= int (numero)
+print(numero)
+print (type(numero))
+#Cambiamos la variable a decimal
+numero=float(numero)
+print(numero)
+print(type(numero))
+#Se convierte a variable compleja
+numero=complex (numero)
+print(numero)
+print(type(numero))
+
+# numero="B"
+# numero= int (numero)
+
+bandera=0
+print(bandera)
+print(type(bandera))
+
+bandera=bool(bandera)
+print(bandera)
+print(type(bandera))
+
---
+++ variables.py
@@ -0,0 +1,25 @@
+#Aqui hay una variable de tipo entero
+numero= 147 
+print(numero)
+print (type(numero)) #Esta variable te arroja el tipo de variable que es
+#Aqui hay una variable de tipo decimal
+numero= 147.5
+print(numero)
+print (type(numero))
+#La variable es complex por ser complejo
+complejo= 1+2j
+print(complejo)
+print(type(complejo))
+#La variable es str por ser cadena de texto
+palabra= "hola"
+print (palabra)
+print(type(palabra))
+#Su variable bool booleano de tipo V o F
+bandera=True
+print(bandera)
+print(type(bandera))
+#Variable sin valor
+vacio=None
+print(vacio)
+print(type(vacio))
+
