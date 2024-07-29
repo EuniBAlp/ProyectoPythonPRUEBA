76f9878d1ddc4c0344aaaaf83cfadc5218965a6e
EuniBAlp <eunice.ba43@gmail.com>
2024-07-29 00:23:05 +0000

    modificacion

--- borrador.py
+++ borrador.py
@@ -1,7 +1,14 @@
-print("Calculadora de Índice de Masa Corporal")
-nombre=input("¿Cuál es tu nombre?")
-ap= input("¿Cuál es tu apellido paterno?")
-am=input("¿Cuál es tu apellido materno?")
+print("Bienvenid@ a la Calculadora de Índice de Masa Corporal")
+print ("Te pediremos unos datos, recuerda leer y contestar de forma precisa.")
+nombre= str (input("¿Cuál es tu nombre?"))
+while not nombre. isalpha (): print ("Nombre inválido, intenta de nuevo por favor.") 
+
+ap= str(input("¿Cuál es tu apellido paterno?"))
+while not ap. isalpha(): print ("Apellido inválido, intenta de nuevo por favor.")
+
+am= str (input("¿Cuál es tu apellido materno?"))
+while not am. isalpha(): ("Apellido inválido, intenta de nuevo por favor.")
+
 edad=int (input("¿Cuántos años tienes?")) 
 p=float (input("¿Cuánto pesas? Utiliza la unidad de medida en kg."))
 e=float (input("¿Cuánto mides? Utiliza la unidad de medida en metros, por favor."))
@@ -9,10 +16,10 @@ print("Hola", nombre,ap,am)#Saluda
 print ("Tu indice de masa corporal es el siguiente")#Agregaimc
 IMC= p/e**2 #Se imprime el IMC
 print ("IMC:"+str(IMC))
-#Se hace validación
-if IMC >=0 and IMC <= 18.5: print ("Peso bajo")
-if IMC >=18.5 and IMC <= 24.9 : print ("Peso normal")
-if IMC >=24.9 and IMC <= 29.9 : print ("Sobrepeso")
-if IMC >=30 and IMC <= 34.9 :print ("Obesidad Tipo I")
-if IMC >=35 and IMC <= 39.9 :print ("Obesidad Tipo II")
-if IMC >= 40: print ("Obesidad Tipo III")
+#Se valida imc
+if IMC <= 18.5: print ("Peso bajo")
+elif IMC >=18.5 and IMC <= 24.9 : print ("Peso normal")
+elif IMC >=24.9 and IMC <= 29.9 : print ("Sobrepeso")
+elif IMC >=30 and IMC <= 34.9 :print ("Obesidad Tipo I")
+elif IMC >=35 and IMC <= 39.9 :print ("Obesidad Tipo II")
+elif IMC >= 40: print ("Obesidad Tipo III")
